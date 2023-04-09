3094c8c71dd12ee2eebe56581b7e1b273f566d0b
Shaketshubham276 <2021ugme023@nitjsr.ac.in>
2023-04-10 04:20:34 +0530

    kam chal rha

--- server.js
+++ server.js
@@ -6,8 +6,13 @@ const express=require("express")
 const expressLayouts=require("express-ejs-layouts")
 const indexRouter=require('./routes/index')
 const authorRouter=require('./routes/authors')
+const bookRouter=require('./routes/books')
 const bodyParser=require('body-parser')
 const mongoose=require("mongoose")
+
+const multer=require("multer")
+//multer helps in storing the files taken by the user
+
 mongoose.connect(process.env.MONGO_URI,{
     useNewUrlParser:true
 })
@@ -17,19 +22,18 @@ db.once('open',()=>console.log("connected to mongo"))
 
 
 const app=express()
-
-
 app.set('views',__dirname +'/views')
 app.set('view engine',"ejs")
 app.set('layout',"layouts/layout")
 
-app.use(bodyParser.urlencoded({limit:'10mb',extended:false}))
 
+app.use(bodyParser.urlencoded({limit:'10mb',extended:false}))
 app.use(expressLayouts)
 app.use(express.static('/public'))
 
 app.use('/',indexRouter)
 app.use('/authors',authorRouter)
+app.use('/books',bookRouter)
 
 
 
