2c8b7d9d4c78bc3f5859de735071919f2f84a8c1
Shaketshubham276 <2021ugme023@nitjsr.ac.in>
2023-04-16 03:29:30 +0530

    done some

--- models/book.js
+++ models/book.js
@@ -1,4 +1,7 @@
 const mongoose=require('mongoose')
+const path=require('path')
+const coverImageBasePath='uploads/bookCovers'
+
 const bookSchema=new mongoose.Schema({
     title:{
         type:String,
@@ -34,4 +37,17 @@ const bookSchema=new mongoose.Schema({
     }
 })
 
+
+// We havent used arrow function because we have to use this property at this point
+
+
+// if we call coverImagePath then the below function will be called
+bookSchema.virtual('coverImagePath').get(function(){
+    if (this.coverImageName!=null)
+    return path.join('/',coverImageBasePath,this.coverImageName)
+    // this.coverImageName corresponds to book that we are looking for inside the coverImageBasePath
+})
+
+
 module.exports=mongoose.model('Book',bookSchema)
+module.exports.coverImageBasePath = coverImageBasePath
--- routes/books.js
+++ routes/books.js
@@ -1,43 +1,99 @@
 const express=require("express")
 const router=express.Router()
 const Book=require('../models/book')
+const multer=require("multer")
 const Author=require('../models/author')
-
+const path=require('path')
+const imageMimeTypes=['image/jpeg','images/png','images/gif']
+// The imageMimeTypes helps to take the input data of which type you have inclueded
+const uploadPath=path.join('public',Book.coverImageBasePath)
+const upload=multer({
+    dest:uploadPath,
+    fileFilter:(req,file,callback)=>{
+        callback(null,imageMimeTypes.includes(file.mimetype))
+    }
+    // file Filter helps to filter which files our server accept
+})
 
 //All books route 
 router.get('/',async (req,res)=>{
-    res.send("hello")
+    try{
+        const books=await Book.find({})
+        res.render('books/index',{
+            books:books,
+            searchOptions:req.query
+        })
+    }
+    catch{
+        
+        res.redirect('/')
+    }
     
 })
 //New Books Route
 router.get('/new',async (req,res)=>{
+    renderNewPage(res,new Book())
+    
+})
+
+
+//Create Book Route
+
+// the below upload.single('cover' ) means that we have to upload a single file name cover
+router.post('/',upload.single('cover'),async (req,res)=>{
+    const fileName=req.file!=null ? req.file.filename:null
+    const book=new Book({
+    title:req.body.title,
+    author:req.body.author,
+    publishDate:new Date(req.body.publishDate),
+    pageCount:req.body.pageCount,
+    description:req.body.description,
+
+    coverImageName:fileName
+
+   })
+   try{
+    const newBook= await book.save()
+    // res.redirect(`books/${newAuthor.id}`)
+    res.redirect(`books`)
+   }
+   catch{
+    if (book.coverImageName!=null){
+        removeBookCover(book.coverImageName)
+    }
+    renderNewPage(res,book,true)
+   }
+})
+
+function removeBookCover(fileName){
+    fs.unlink(path.join(uploadPath,fileName),err=>{
+        if (err) console.error(err)
+
+    // fs helps to unlink the book folder if the file is added then it gets unloaded
+    })
+}
+
+async function renderNewPage (res,book,hasError=false){
     try{
-        
+            
         const authors=await Author.find({})
         const book= new Book()
-        res.render('books/new',{
+        const params={
             authors:authors,
             book:book
-        })
+        }
+        if (hasError) params.errorMessage=`Error Creating BOOK`
+        // params.errorMessage creates an error Message and shows error creating
+        
+        res.render('books/new',params)
             
         
     }
     catch{
         res.redirect('/books')
     }
-    
-})
 
+}
 
-//Create Book Route
-router.post('/',async(req,res)=>{
-   const book=new Book({
-    title:req.body.title,
-    author:req.body.author,
-    publishDate:new Date(req.body.publishDate),
-    pageCount:req.body.pageCount,
-    description:req.body.description
-   })
-})
 
 module.exports=router
